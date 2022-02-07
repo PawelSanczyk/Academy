37d47df53f548f191227d4901ef42e1eb915281c
JaSemTvojOtec <adam.gabrysiak@gmail.com>
2022-02-07 23:16:43 +0100

    Update
    Dodano dodatkowe pola formularza.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,42 +3,50 @@ import { useEffect, useState } from "react";
 
 interface IKandydat  {
   imie: string
-//   nazwisko: string
-//   email: string
-//   plec: string
-//   telefon: string
+  nazwisko: string | undefined //Musiałem dodać typ "undefined" bo inaczej wywalało błąd typu w setObiekt w handleButton
+  email: string | undefined //Musiałem dodać typ "undefined" bo inaczej wywalało błąd typu w setObiekt w handleButton
+  plec: string | undefined //Musiałem dodać typ "undefined" bo inaczej wywalało błąd typu w setObiekt w handleButton
+  telefon: string | undefined //Musiałem dodać typ "undefined" bo inaczej wywalało błąd typu w setObiekt w handleButton
 }
 
 
 const kandydat: IKandydat = {  
   imie:"Adam",
-  // nazwisko:"Gabrysiak",
-  // email:"adam.gabrysiak@gmail.com",
-  // plec:"M",
-  // telefon:"555-444-333",
+  nazwisko:"Gabrysiak",
+  email:"adam.gabrysiak@gmail.com",
+  plec:"M",
+  telefon:"555-444-333",
 }
 
 const kandydatDrugi: IKandydat = {  
   imie:"Pawel",
-  // nazwisko:"sanczyk",
-  // email:"p.sanczyk@gmail.com",
-  // plec:"M",
-  // telefon:"555-444-333",
+  nazwisko:"sanczyk",
+  email:"p.sanczyk@gmail.com",
+  plec:"M",
+  telefon:"555-444-333",
 }
 
 
 const HomePage = () => {
+
   const [imie, setImie] = useState<string>()
+  const [nazwisko, setNazwisko] = useState<string>()
+  const [email, setEmail] = useState<string>()
+  const [plec, setPlec] = useState<string>()
+  const [telefon, setTelefon] = useState<string>()
+
   const [obiekt, setObiekt] = useState<IKandydat[]>([kandydat])
 
 
-const handleButton = () => {
+const handleButton = () => {            //Dodaje dopiero po drugim wciśnięciu przycisku
   {console.log("wykonuje func handle", imie)}
 
-   if (imie)  {
-     setObiekt([...obiekt, {imie: imie}])
+   if (imie)  {                        //Jak ustalić warunek od obecności wszystkich zmiennych?
+    console.log("======> Dodaję nową pozycję", imie)
+     setObiekt([...obiekt, {imie: imie, nazwisko: nazwisko, email: email, plec: plec, telefon: telefon}])
      window.localStorage.setItem("pacjenci", JSON.stringify(obiekt))
    }
+   
   {console.log(obiekt)}
 }
 
@@ -59,8 +67,33 @@ useEffect(()=>{
 
     return (
   <div>
-    <input name="inputPierwszy" onChange={value => setImie(value.target.value)}></input>
-    <button  onClick={handleButton}>Dodaj Kandydata</button>
+    <label htmlFor="1">Imię:</label><br></br>
+    <input id ="1" name="inputPierwszy" onChange={value => setImie(value.target.value)}></input>
+    <br></br><br></br>
+
+    <label htmlFor="2">Nazwisko:</label><br></br>
+    <input id ="2" name="inputDrugi" onChange={value => setNazwisko(value.target.value)}></input>
+    <br></br><br></br>
+
+    <label htmlFor="3">Email:</label><br></br>
+    <input id ="3" name="inputTrzeci" onChange={value => setEmail(value.target.value)}></input>
+    <br></br><br></br>
+
+    <label htmlFor="4">Płeć:</label><br></br>
+    {/* <input id ="4" name="inputCzwarty" onChange={value => setPlec(value.target.value)}></input>  */}
+    
+
+    <select id="4" onChange={value => setPlec(value.target.value)}>
+    <option value="M">Mężczyzna</option>
+    <option value="K">Kobieta</option>
+    </select>
+    <br></br><br></br>
+
+    <label htmlFor="5">Telefon:</label><br></br>
+    <input id ="5" name="inputPiaty" onChange={value => setTelefon(value.target.value)}></input>
+    <br></br><br></br>
+
+    <button  onClick={handleButton}>Dodaj Kandydata</button><br></br>
     <button onClick={czyscicielStorage}>Wyczysc</button>
   </div>
     );
